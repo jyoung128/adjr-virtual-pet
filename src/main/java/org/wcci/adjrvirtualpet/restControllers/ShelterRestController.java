8bb0c0049033b26e96334ca0791803f2ea022a72
dawsonblack <134227563+dawsonblack@users.noreply.github.com>
2023-08-01 12:05:19 -0400

    saving progress

--- src/main/java/org/wcci/adjrvirtualpet/restControllers/ShelterRestController.java
+++ src/main/java/org/wcci/adjrvirtualpet/restControllers/ShelterRestController.java
@@ -25,132 +25,108 @@ import org.wcci.adjrvirtualpet.services.ShelterService;
 
 @RestController
 public class ShelterRestController {
-    public static final String LIST_ALL_STUDENTS = "listAllStudents";
-    public static final String LIST_ALL_COURSES = "listAllCourses";
-    public static final String COURSES_FOR_STUDENT = "coursesForStudent";
+    public static final String LIST_ALL_DOGS = "listAllDogs";
+    public static final String LIST_ALL_CATS = "listAllCats";
 
     final private ShelterService shelterService;
 
-    public ShelterRestController(@Autowired ShelterService studentRepo) {
-        this.shelterService = studentRepo;
+    public ShelterRestController(@Autowired ShelterService shelterService) {
+        this.shelterService = shelterService;
     }
 
-    // Return all students
-    // curl -s http://localhost:8080/api/students
-    // This *reads* from the database and is the "R" in CRUD
-    @GetMapping("/api/students")
-    public CollectionModel<EntityModel<Student>> getStudents() {
-        List<EntityModel<Student>> students = this.shelterService.studentStream()
-                .map(student -> EntityModel.of(student))
+    @GetMapping("/api/dogs")
+    public CollectionModel<EntityModel<Dog>> getDogs() {
+        List<EntityModel<Dog>> dogs = this.shelterService.dogStream()
+                .map(dog -> EntityModel.of(dog))
                 .collect(Collectors.toList());
-        return CollectionModel.of(students);
+        return CollectionModel.of(dogs);
     }
 
-    @GetMapping("/api/courses")
-    public CollectionModel<EntityModel<Course>> getCourses() {
-        List<EntityModel<Course>> courses = this.shelterService.courseStream()
-                .map(course -> EntityModel.of(course))
+    @GetMapping("/api/cats")
+    public CollectionModel<EntityModel<Cat>> getCats() {
+        List<EntityModel<Cat>> cats = this.shelterService.catStream()
+                .map(cat -> EntityModel.of(cat))
                 .collect(Collectors.toList());
-        return CollectionModel.of(courses);
+        return CollectionModel.of(cats);
     }
 
-    // Return one students
-    // curl -s http://localhost:8080/api/students/1
-    // This *reads* from the database and is the "R" in CRUD
-    @GetMapping("/api/students/{student_id}")
-    public EntityModel<Student> getStudent(@PathVariable final Long student_id) {
-        final Student student = shelterService.findStudent(student_id);
-        return EntityModel.of(student,
-                linkTo(methodOn(ShelterRestController.class).getStudents()).withRel(LIST_ALL_STUDENTS),
-                linkTo(methodOn(ShelterRestController.class).getStudentCourses(student_id)).withRel(COURSES_FOR_STUDENT),
-                linkTo(methodOn(ShelterRestController.class).getStudent(student_id)).withSelfRel());
+    @GetMapping("/api/dogs/{dog_id}")
+    public EntityModel<Dog> getDog(@PathVariable final Long dog_id) {
+        final Dog dog = shelterService.findDog(dog_id);
+        return EntityModel.of(dog,
+                linkTo(methodOn(ShelterRestController.class).getDogs()).withRel(LIST_ALL_DOGS),
+                linkTo(methodOn(ShelterRestController.class).getDog(dog_id)).withSelfRel());
     }
 
-    @GetMapping("/api/courses/{course_id}")
-    public EntityModel<Course> getCourse(@PathVariable final Long course_id) {
-        final Course course = shelterService.findCourse(course_id);
-        return EntityModel.of(course,
-                linkTo(methodOn(ShelterRestController.class).getCourses()).withRel(LIST_ALL_COURSES),
-                linkTo(methodOn(ShelterRestController.class).getCourse(course_id)).withSelfRel());
+    @GetMapping("/api/cats/{cat_id}")
+    public EntityModel<Cat> getCat(@PathVariable final Long cat_id) {
+        final Cat cat = shelterService.findCat(cat_id);
+        return EntityModel.of(cat,
+                linkTo(methodOn(ShelterRestController.class).getCats()).withRel(LIST_ALL_CATS),
+                linkTo(methodOn(ShelterRestController.class).getDog(cat_id)).withSelfRel());
     }
 
-    // Add a student
-    // curl -s -X POST http://localhost:8080/api/students -d '{"name":
-    // "marshall2"}' -H 'Content-Type: application/json'
-    // This *creates* a database record and is the "C" in CRUD
-    @PostMapping("/api/students")
-    public EntityModel<Student> newStudent(@RequestBody final Student student) {
-        return EntityModel.of(shelterService.writeToDatabase(student),
-                linkTo(methodOn(ShelterRestController.class).getStudent(student.studentID)).withSelfRel(),
-                linkTo(methodOn(ShelterRestController.class).getStudentCourses(student.studentID))
-                        .withRel(COURSES_FOR_STUDENT),
-                linkTo(methodOn(ShelterRestController.class).getStudents()).withRel(LIST_ALL_STUDENTS));
+    @PostMapping("/api/dogs")
+    public EntityModel<Dog> newDog(@RequestBody final Dog dog) {
+        return EntityModel.of(shelterService.writeToDatabase(dog),
+                linkTo(methodOn(ShelterRestController.class).getDog(dog.dogID)).withSelfRel(),
+                linkTo(methodOn(ShelterRestController.class).getDogs()).withRel(LIST_ALL_DOGS));
     }
 
-    @PostMapping("/api/courses")
-    public EntityModel<Course> newCourse(@RequestBody final Course course) {
-        return EntityModel.of(shelterService.writeToDatabase(course),
-                linkTo(methodOn(ShelterRestController.class).getCourse(course.getCourseID())).withSelfRel(),
-                linkTo(methodOn(ShelterRestController.class).getCourses()).withRel(LIST_ALL_COURSES));
+    @PostMapping("/api/cats")
+    public EntityModel<Cat> newCat(@RequestBody final Cat cat) {
+        return EntityModel.of(shelterService.writeToDatabase(cat),
+                linkTo(methodOn(ShelterRestController.class).getCat(cat.catID)).withSelfRel(),
+                linkTo(methodOn(ShelterRestController.class).getCats()).withRel(LIST_ALL_CATS));
     }
 
-    // Remove a student
-    // curl -s -X DELETE http://localhost:8080/api/students/51
-    // This *delete* a database record and is the "D" in CRUD
-    @DeleteMapping("/api/students/{student_id}")
-    public void deleteStudent(@PathVariable long student_id) {
-        shelterService.deleteById(student_id);
+    @DeleteMapping("/api/dogs/{dog_id}")
+    public void deleteDog(@PathVariable long dog_id) {
+        shelterService.deleteDogById(dog_id);
+    }
+
+    @DeleteMapping("/api/cats/{cat_id}")
+    public void deleteCat(@PathVariable long cat_id) {
+        shelterService.deleteCatById(cat_id);
     }
 
     // Talk to the Product Owner before changing this
-    @DeleteMapping("/api/students")
-    public void deleteAllStudents() {
+    @DeleteMapping("/api/dogs")
+    public void deleteAllDogs() {
         throw new ResponseStatusException(HttpStatus.FORBIDDEN);
     }
 
-    // Update a student
-    // curl -s -X PUT http://localhost:8080/api/students -d '{"studentID": 102,
-    // "name": "marshall2"}' -H 'Content-Type: application/json'
-    // This *updates* a database record and is the "C" in CRUD
-    @PutMapping("/api/students/{student_id}")
-    public EntityModel<Student> updateStudent(
-            @PathVariable final long student_id, // the name of the parameter (student_id) must match "{student_id}" in
+    @DeleteMapping("/api/cats")
+    public void deleteAllCats() {
+        throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+    }
+
+    @PutMapping("/api/dogs/{dog_id}")
+    public EntityModel<Dog> updateDog(
+            @PathVariable final long dog_id, // the name of the parameter (dog_id) must match "{dog_id}" in
                                                  // the line above
-            @RequestBody final Student student) {
-        // Update the student if that is the right thing to do
-        final Student databaseStudent = shelterService.updateStudent(student, student_id);
+            @RequestBody final Dog dog) {
+        // Update the dog if that is the right thing to do
+        final Dog databaseDog = shelterService.updateDog(dog, dog_id);
 
-        // Return the modified database student
-        return EntityModel.of(databaseStudent,
-                linkTo(methodOn(ShelterRestController.class).getStudent(student.studentID)).withSelfRel());
+        // Return the modified database dog
+        return EntityModel.of(databaseDog,
+                linkTo(methodOn(ShelterRestController.class).getDog(dog.dogID)).withSelfRel());
     }
 
-    @PutMapping("/api/courses/{course_id}")
-    public EntityModel<Course> updateCourse(
-            @PathVariable final long course_id, // the name of the parameter (student_id) must match "{student_id}" in
-                                                // the line above
-            @RequestBody final Course course) {
-        // Update the student if that is the right thing to do
-        final Course databaseCourse = shelterService.updateCourse(course, course_id);
-
-        // Return the modified database student
-        return EntityModel.of(databaseCourse,
-                linkTo(methodOn(ShelterRestController.class).getCourse(course.getCourseID())).withSelfRel());
-    }
-
-    @GetMapping("/api/students/{student_id}/courses")
-    public CollectionModel<EntityModel<Course>> getStudentCourses(@PathVariable final Long student_id) {
-        List<EntityModel<Course>> courses = this.shelterService.courseStreamForStudent(student_id)
+    @GetMapping("/api/dogs/{dog_id}/courses")
+    public CollectionModel<EntityModel<Course>> getDogCourses(@PathVariable final Long dog_id) {
+        List<EntityModel<Course>> courses = this.shelterService.courseStreamForDog(dog_id)
                 .map(course -> EntityModel.of(course))
                 .collect(Collectors.toList());
         return CollectionModel.of(courses);
     }
 
-    @PostMapping("/api/students/{student_id}/courses")
-    public EntityModel<Course> newCourseForStudent(
-            @PathVariable final Long student_id,
+    @PostMapping("/api/dogs/{dog_id}/courses")
+    public EntityModel<Course> newCourseForDog(
+            @PathVariable final Long dog_id,
             @RequestBody final Course course) {
-        shelterService.addCourseToStudent(student_id, course);
+        shelterService.addCourseToDog(dog_id, course);
         return EntityModel.of(course);
     }
 }
