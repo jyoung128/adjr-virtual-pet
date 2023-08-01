f119dd70e5909b7746b92b2d96efa3db32b05462
dawsonblack <134227563+dawsonblack@users.noreply.github.com>
2023-08-01 16:43:54 -0400

    testing out get and post

--- src/main/webapp/javascript/Test.jsx
+++ src/main/webapp/javascript/Test.jsx
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from "react";
 
 export default function Test() {
 
-  const getDog = (ID) => {
+  const getDog = () => {
+    const ID = document.getElementById('text').value;
+    console.log(ID);
     fetch(`api/organicDogs/{ID}`, { method: "GET", cache: "default" })
       .then((response) => response.json())
       .then((responseBody) => {
@@ -10,9 +12,39 @@ export default function Test() {
         });
   };
 
+  const newDog = () => {
+
+    const dogData = {
+      name: "kjvndf",
+      ageInDays: 69,
+      Hunger: 0,
+      thirst: 0,
+      energy: 10,
+      mood: 7,
+      cageCleanliness: 100,
+      hasBeenRecentlyWalked: false,
+    };
+
+    fetch('api/organicDogs', { method: "POST", headers: {
+        "Content-Type": "application/json",
+      }, body: JSON.stringify(dogData), })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        console.log("Activity saved successfully!");
+      })
+      .catch((error) => {
+        console.error("Error saving activity:", error);
+      });
+  };
+
   return (
     <div>
       <p>AHHHHHHHHHH</p>
+      <button onClick={newDog}>New Dog</button>
+      <input type="text" id='text'/>
+      <button onClick={getDog}>Get Dog</button>
     </div>
   );
 }
