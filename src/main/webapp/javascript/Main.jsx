ef91802a26d1cd8127e7a67187a1cc2ddd35bcb5
dawsonblack <134227563+dawsonblack@users.noreply.github.com>
2023-08-01 15:30:14 -0400

    Adding test page to react

--- src/main/webapp/javascript/Main.jsx
+++ src/main/webapp/javascript/Main.jsx
@@ -3,11 +3,14 @@ import { createRoot }  from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
 import '../css/style.css';
 
+import HelloWorld from './Test';
+
 function Layout() {
     return (
         <>
             <nav>
                 <Link to="/">Main</Link>
+                <Link to="/HelloWorld">Hello World</Link>
             </nav>
             <Outlet />
         </>
@@ -20,7 +23,7 @@ function Main(){
             <BrowserRouter>
                 <Routes>
                     <Route path="/app4?/src?/main?/resources?/static?/index.html?" element={<Layout />}>
-                        
+                        <Route path="HelloWorld" element={<HelloWorld />} />
                     </Route>
                 </Routes>
             </BrowserRouter>
---
+++ src/main/webapp/javascript/Test.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+export default function Bored() {
+  return (
+    <div>
+      <p>AHHHHHHHHHH</p>
+    </div>
+  );
+}
