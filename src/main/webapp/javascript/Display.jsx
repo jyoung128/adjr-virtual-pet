e4ecc5f69ab58e773649d846e3babe859730397a
Abdikarim Farah <abdikarimfarah12@gmail.com>
2023-08-02 15:37:48 -0400

    Merge branch 'master' of https://github.com/jyoung128/adjr-virtual-pet

--- src/main/webapp/javascript/Display.jsx
+++ src/main/webapp/javascript/Display.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 export default function Display() {
   let [allOrganicDogs, setAllOrganicDogs] = useState([]);
   let [allOrganicCats, setAllOrganicCats] = useState([]);
-  let [organicShelters, setOrganicShelters] = useState([])
+  let [organicShelters, setOrganicShelters] = useState([]);
 
   const getCats = () => {
     fetch(`/api/organicCats`, { method: "GET", cache: "default" })
@@ -19,39 +19,42 @@ export default function Display() {
     return () => {};
   };
 
-//   const getShelters = () => {
-//     fetch("/api/organicDogs/{organicDog_id}", { method: "GET", cache: "default" })
-//       .then((response) => response.json())
-//       .then((responseBody) => setAllOrganicShelters(responseBody.results));
-//     return () => {};
-//   };
-
+  //   const getShelters = () => {
+  //     fetch("/api/organicDogs/{organicDog_id}", { method: "GET", cache: "default" })
+  //       .then((response) => response.json())
+  //       .then((responseBody) => setAllOrganicShelters(responseBody.results));
+  //     return () => {};
+  //   };
 
   return (
     <div>
-        <div id="cats">
+      <div id="cats">
         <button onClick={getCats}>Show All Cats</button>
-      <ul>
-        {allOrganicCats.map((oneCat) => (
-          <OrganicCat key={oneCat.petId} organicCat={oneCat} />
-        ))}
-      </ul>
+        <ul>
+          {allOrganicCats.map((oneCat) => (
+            <OrganicCat key={oneCat.petId} organicCat={oneCat} />
+          ))}
+        </ul>
       </div>
       <div id="dogs">
-      <button onClick={getDogs}> Show All dogs</button>
-      <ul>
-        {allOrganicDogs.map((oneDog) => (
-          <OrganicDog key={oneDog.petId} organicDog={oneDog} />
-        ))}
-      </ul>
+        <button onClick={getDogs}> Show All dogs</button>
+        <p>{JSON.stringify(allOrganicDogs)}</p>
+        <ul>
+          {allOrganicDogs.map((oneDog) => (
+            <OrganicDog key={oneDog.petId} organicDog={oneDog} />
+          ))}
+        </ul>
       </div>
       <div id="shelter">
-      {/* <button onClick={getShelters}>All Shelters</button> */}
-      <ul>
-        {organicShelters.map((oneShelter) => (
-            <OrganicShelter key={oneShelter.shelterId} organicShelter={oneShelter} />
-        ))}
-      </ul>
+        {/* <button onClick={getShelters}>All Shelters</button> */}
+        <ul>
+          {organicShelters.map((oneShelter) => (
+            <OrganicShelter
+              key={oneShelter.shelterId}
+              organicShelter={oneShelter}
+            />
+          ))}
+        </ul>
       </div>
     </div>
   );
@@ -84,13 +87,12 @@ export default function Display() {
     );
   }
 
-  function OrganicShelter({organicShelter}){
+  function OrganicShelter({ organicShelter }) {
     return (
-        <ul>
-            <li key={organicShelter.shelterId}></li>
-            <li>Organic Pets:{organicShelter.allPets}</li>
-        </ul>
-    )
+      <ul>
+        <li key={organicShelter.shelterId}></li>
+        <li>Organic Pets:{organicShelter.allPets}</li>
+      </ul>
+    );
   }
-
 }
