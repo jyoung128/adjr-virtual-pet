8952b1f8d967f8c0bd435d862bfd337dc259d08a
Abdikarim Farah <abdikarimfarah12@gmail.com>
2023-08-03 09:39:47 -0400

    display

--- src/main/webapp/javascript/Display.jsx
+++ src/main/webapp/javascript/Display.jsx
@@ -5,54 +5,6 @@ export default function Display() {
   let [allOrganicCats, setAllOrganicCats] = useState([]);
   let [organicShelters, setOrganicShelters] = useState([]);
 
-  const getCats = () => {
-    fetch(`/api/organicCats`, { method: "GET", cache: "default" })
-      .then((response) => response.json())
-      .then((responseBody) => setAllOrganicCats(responseBody));
-    return () => {};
-  };
-
-  const getDogs = () => {
-    fetch(`/api/organicDogs`, { method: "GET", cache: "default" })
-      .then((response) => response.json())
-      .then((responseBody) => setAllOrganicDogs(responseBody));
-    return () => {};
-  };
-
-  //   const getShelters = () => {
-  //     fetch("/api/organicDogs/{organicDog_id}", { method: "GET", cache: "default" })
-  //       .then((response) => response.json())
-  //       .then((responseBody) => setAllOrganicShelters(responseBody.results));
-  //     return () => {};
-  //   };
-  if (allOrganicCats && allOrganicCats._embedded) {
-    return (
-      <div>
-        <button onClick={getCats}>Show All Cats</button>
-        <button onClick={getDogs}> Show All dogs</button>
-        <ul>
-          {allOrganicCats["_embedded"]["organicCatList"].map((oneCat) => (
-            <OrganicCat key={oneCat.petId} organicCat={oneCat} />
-          ))}
-        </ul>
-      </div>
-    );
-  }
-
-  else if (allOrganicDogs && allOrganicDogs._embedded) {
-    return (
-      <div>
-        <button onClick={getDogs}> Show All dogs</button>
-        <button onClick={getCats}>Show All Cats</button>
-        <ul>
-          {allOrganicDogs["_embedded"]["organicDogList"].map((oneDog) => (
-            <OrganicDog key={oneDog.petID} organicDog={oneDog} />
-          ))}
-        </ul>
-      </div>
-    );
-  }
-
   return (
     <div>
       <div id="cats">
@@ -74,7 +26,56 @@ export default function Display() {
       </div>
     </div>
   );
+
+function getCats() {
+  fetch(`/api/organicCats`, { method: "GET", cache: "default" })
+    .then((response) => response.json())
+    .then((responseBody) => setAllOrganicCats(responseBody));
+  return () => {};
 }
+
+if (allOrganicCats && allOrganicCats._embedded) {
+  return (
+    <div>
+      <button onClick={getCats}>Show All Cats</button>
+      <button onClick={getDogs}> Show All dogs</button>
+      <ul>
+        {allOrganicCats["_embedded"]["organicCatList"].map((oneCat) => (
+          <OrganicCat key={oneCat.petId} organicCat={oneCat} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+}
+
+
+function getDogs() {
+  fetch(`/api/organicDogs`, { method: "GET", cache: "default" })
+    .then((response) => response.json())
+    .then((responseBody) => setAllOrganicDogs(responseBody));
+  return () => {};
+}
+if (allOrganicDogs && allOrganicDogs._embedded) {
+  return (
+    <div>
+      <button onClick={getDogs}> Show All dogs</button>
+      <button onClick={getCats}>Show All Cats</button>
+      <ul>
+        {allOrganicDogs["_embedded"]["organicDogList"].map((oneDog) => (
+          <OrganicDog key={oneDog.petID} organicDog={oneDog} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+//   const getShelters = () => {
+//     fetch("/api/organicDogs/{organicDog_id}", { method: "GET", cache: "default" })
+//       .then((response) => response.json())
+//       .then((responseBody) => setAllOrganicShelters(responseBody.results));
+//     return () => {};
+//   };
 
 function OrganicCat({ organicCat }) {
   return (
@@ -108,3 +109,4 @@ function OrganicShelter({ organicShelter }) {
     </ul>
   );
 }
+
