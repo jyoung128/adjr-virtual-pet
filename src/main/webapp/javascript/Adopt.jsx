c05beeab44ffe4d895d982c02d612d27a35256f7
JohnsonRobertE <JohnsonRobertE809@gmail.com>
2023-08-04 10:09:21 -0400

    Feed button

--- src/main/webapp/javascript/Adopt.jsx
+++ src/main/webapp/javascript/Adopt.jsx
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from "react";
 
 export default function Adopt() {
   const [id, setId] = useState("");
-  const [organicPetName, setOrganicDogName] = useState("");
   const [organicShelterName, setOrganicShelterName] = useState("");
+  const [organicDog, setOrganicDog] = useState("");
+  const [organicCat, setOrganicCat] = useState("");
 
   const getDog = () => {
     fetch(`api/organicDogs/${id}`, { method: "GET", cache: "default" })
       .then((response) => response.json())
-      .then((responseBody) => setOrganicDogName(responseBody.name));
+      .then((responseBody) => {
+        setOrganicDog(responseBody);
+      });
   };
 
   const deleteDog = () => {
@@ -29,7 +32,7 @@ export default function Adopt() {
   const getCat = () => {
     fetch(`api/organicCats/${id}`, { method: "GET", cache: "default" })
       .then((response) => response.json())
-      .then((responseBody) => setOrganicDogName(responseBody.name));
+      .then((responseBody) => setOrganicCat(responseBody));
   };
 
   const deleteCat = () => {
@@ -72,6 +75,54 @@ export default function Adopt() {
     setId(target.value);
   };
 
+  const updateDogHunger = () => {
+    const data = {
+      ...organicDog,
+      hunger: 30,
+    };
+
+    fetch(`api/organicDogs/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        console.log("Dog name updated successfully!");
+      })
+      .catch((error) => {
+        console.error("Error updating dog name:", error);
+      });
+  };
+
+  const updateCatHunger = () => {
+    const data = {
+      ...organicCat,
+      hunger: 30,
+    };
+
+    fetch(`api/organicCats/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        console.log("Dog name updated successfully!");
+      })
+      .catch((error) => {
+        console.error("Error updating dog name:", error);
+      });
+  };
+
   return (
     <div>
       <div>
@@ -86,7 +137,7 @@ export default function Adopt() {
         </form>
       </div>
       <div>
-        <div>{organicPetName}</div>
+        <div>{JSON.stringify(organicCat)}</div>
         <div>{organicShelterName}</div>
         <button onClick={getDog}>See Dogs Name by ID</button>
         <button onClick={getCat}>See Cats Name by ID</button>
@@ -94,6 +145,8 @@ export default function Adopt() {
         <button onClick={deleteDog}>Delete Dog</button>
         <button onClick={deleteCat}>Delete Cat</button>
         <button onClick={deleteShelter}>Delete Shelter</button>
+        <button onClick={updateDogHunger}>Feed</button>
+        <button onClick={updateCatHunger}>Feed</button>
       </div>
     </div>
   );
